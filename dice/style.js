126e30dc6c6d947a728538dd8d99e510e1f2354d
crypt37 <81977500+crypt37@users.noreply.github.com>
2021-06-16 12:02:02 +0545

    Delete dice directory

--- dice/style.js
+++
@@ -1,5 +0,0 @@
-var player_1 = Math.floor(Math.random() * 5) + 1;
-var player_2 = Math.floor(Math.random() * 5) + 1;
-var x = document.getElementsByClassName("img");
-x[0].src = "images/" + player_1 + ".png";
-x[1].src = "images/" + player_2 + ".png";
