adf4b909ddc2b97e70af2c42469a1de70afda206
crypt37 <81977500+crypt37@users.noreply.github.com>
2021-06-16 12:11:18 +0545

    Add files via upload

---
+++ dice/script.js
@@ -0,0 +1,23 @@
+var player_1 = Math.floor(Math.random() * 5) + 1;
+var player_2 = Math.floor(Math.random() * 5) + 1;
+var x = document.getElementsByClassName("img");
+x[0].src = "images/" + player_1 + ".png";
+x[1].src = "images/" + player_2 + ".png";
+
+
+
+
+if ( player_1>player_2)
+{
+   document.getElementById("h1").textContent="player 1 WON ";
+
+}
+ else if ( player_1<player_2)
+{
+    document.getElementById("h1").textContent="player 2 WON ";
+}
+else  if  ( player_1===player_2)
+{
+    document.getElementById("h1").textContent=" DRAW ";
+
+}
